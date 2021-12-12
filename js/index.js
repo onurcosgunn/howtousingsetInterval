3361ffc8bf8efd874ba0f2201f85ba0775ef4e0b
Onur COSGUN <36514819+onurcosgunn@users.noreply.github.com>
2021-12-13 02:22:45 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var value;
+var second = 11;
+
+function StopSecond() {
+  window.clearInterval(value);
+}
+
+function secondStart() {
+  second--;
+  if (second >= 0) {
+    document.getElementById('second').innerHTML = second;
+  } else {
+    window.clearInterval(value);
+    document.getElementById('uyari').innerHTML = "Your period is over!";
+  }
+}
+var value = window.setInterval('secondStart()', 1000);
